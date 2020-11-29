596428f4708731fdb7dc77f6bd73869c2948c8b9
Javall <50710716+Javall113@users.noreply.github.com>
2020-11-30 01:52:55 +0200

    Add files via upload

---
+++ Laboratorna_5/LB5_Number1/SquareSolution/Point.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LB5_Number1.SquareSolution
+{
+    class Point
+    {
+        public Point() { }
+        public Point(double X, double Y, double Length)
+        {
+            this.X = X+311d;
+            this.Y = -Y+240d;
+            this.Length = Length;
+        }
+
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Length { get; set; }
+
+        public Point Left
+        {
+            get
+            {
+                Point LeftPoint = new Point();
+                LeftPoint.X = X-10d;
+                LeftPoint.Y = Y;
+                LeftPoint.Length = Length;
+                return LeftPoint;
+            }
+        }
+        public Point Right
+        {
+            get
+            {
+                Point RightPoint = new Point();
+                RightPoint.X = X+10d;
+                RightPoint.Y = Y;
+                RightPoint.Length = Length;
+                return RightPoint;
+            }
+        }
+        public Point UP
+        {
+            get
+            {
+                Point UpPoint = new Point();
+                UpPoint.X = X;
+                UpPoint.Y = Y-10d;
+                UpPoint.Length = Length;
+                return UpPoint;
+            }
+        }
+        public Point Down
+        {
+            get
+            {
+                Point DownPoint = new Point();
+                DownPoint.X = X;
+                DownPoint.Y = Y+10d;
+                DownPoint.Length = Length;
+                return DownPoint;
+            }
+        }
+    }
+}
---
+++ Laboratorna_5/LB5_Number1/SquareSolution/Square.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Media.Animation;
+
+namespace LB5_Number1.SquareSolution
+{
+    class Square
+    {
+        public Square() { }
+        public Square(Point BasePoint)
+        {
+            this.BasePoint = BasePoint;
+        }
+        public Point BasePoint;
+        //Получение точек
+        public virtual Point GetPointA()
+        {
+            return BasePoint;
+        }
+        public virtual Point GetPointB()
+        {
+            Point pointB = new Point();
+            pointB.X = BasePoint.X + BasePoint.Length;
+            pointB.Y = BasePoint.Y;
+            pointB.Length = BasePoint.Length;
+
+            return pointB;
+        }
+        public virtual Point GetPointC()
+        {
+            Point pointC = new Point();
+            pointC.X = BasePoint.X + BasePoint.Length;
+            pointC.Y = BasePoint.Y + BasePoint.Length;
+
+            return pointC;
+        }
+        public virtual Point GetPointD()
+        {
+            Point pointD = new Point();
+            pointD.X = BasePoint.X;
+            pointD.Y = BasePoint.Y + BasePoint.Length;
+
+            return pointD;
+        }
+
+        //Передвижение фигуры
+        public Square MoveLeft()
+        {
+            return new Square(BasePoint.Left);
+        }
+        public Square MoveRight()
+        {
+            return new Square(BasePoint.Right);
+        }
+        public Square MoveUP()
+        {
+            return new Square(BasePoint.UP);
+        }
+        public Square MoveDown()
+        {
+            return new Square(BasePoint.Down);
+        }
+    }
+}
